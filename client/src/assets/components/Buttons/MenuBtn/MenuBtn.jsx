9df650c7bfe62274c6f64254155e4751049a6b84
dra <nd.mirica@gmail.com>
2025-07-08 18:04:03 +0300

    manu button added

---
+++ client/src/assets/components/Buttons/MenuBtn/MenuBtn.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const MenuBtn = ({
+  containerWidth = 25,
+  color = "#fbfbff",
+  barHeight = 2,
+  barBorderRadius = 99,
+  barMargins = "6px 0 6px 0px",
+  barTransition = "200ms all linear",
+  isMenuOpen = () => {},
+  setIsMenuOpen = () => {},
+}) => {
+  const defaultBarStyle = {
+    backgroundColor: color,
+    height: barHeight + "px",
+    display: "block",
+    borderRadius: barBorderRadius + "px",
+    margin: barMargins,
+    transition: barTransition,
+    transform: "rotate(0) translateY(0)",
+    width: "100%",
+  };
+
+  const topBarStyle = {
+    transform: "rotate(45deg) translateY(11px)",
+    margin: "6px 0 6px 9px",
+  };
+
+  const middleBarStyle = {
+    width: "0px",
+  };
+
+  const bottomBarStyle = {
+    transform: "rotate(-45deg) translateY(-12px)",
+    margin: "6px 0 6px 9px",
+  };
+
+  return (
+    <div
+      className="nav-menu-btn"
+      style={{ width: containerWidth + "px", cursor: "pointer", zIndex: "100" }}
+      onClick={() => {
+        setIsMenuOpen(!isMenuOpen);
+      }}
+    >
+      <span
+        style={
+          isMenuOpen
+            ? { ...defaultBarStyle, ...topBarStyle }
+            : { ...defaultBarStyle }
+        }
+      ></span>
+      <span
+        style={
+          isMenuOpen
+            ? { ...defaultBarStyle, ...middleBarStyle }
+            : { ...defaultBarStyle }
+        }
+      ></span>
+      <span
+        style={
+          isMenuOpen
+            ? { ...defaultBarStyle, ...bottomBarStyle }
+            : { ...defaultBarStyle }
+        }
+      ></span>
+    </div>
+  );
+};
+
+export default MenuBtn;
--- client/src/assets/components/Navbar/MenuContainer/MenuContainer.jsx
+++ client/src/assets/components/Navbar/MenuContainer/MenuContainer.jsx
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 import Logo from "../../Logo/Logo";
 import CartButton from "../../Buttons/CartButton/CartButton";
 import AccountButton from "../../Buttons/AccountButton/AccountButton";
+import MenuBtn from "../../Buttons/MenuBtn/MenuBtn";
+import { useAppContext } from "../../../../context";
 
 const MenuContainer = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { width } = useAppContext();
   return (
     <div className="navbar-menu-container">
       <Logo isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
       <CartButton />
       <AccountButton />
-      <p>menu</p>
+      {width < 960 ? (
+        <MenuBtn isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+      ) : null}
     </div>
   );
 };
